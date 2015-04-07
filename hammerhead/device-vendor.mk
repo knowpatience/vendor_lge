76b97e47b44826c1b48ea4940ee5c58ed3e815cf
knowpatience <know.patience@gmail.com>
2015-04-07 13:50:53 -0400

    update binaries for 5.1.0_r3

--- hammerhead/device-vendor.mk
+++ hammerhead/device-vendor.mk
@@ -20,6 +20,5 @@ LOCAL_STEM := hammerhead/device-partial.mk
 $(call inherit-product-if-exists, vendor/broadcom/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/lge/$(LOCAL_STEM))
 $(call inherit-product-if-exists, vendor/qcom/$(LOCAL_STEM))
-$(call inherit-product-if-exists, vendor/google/device-partial.mk)
 $(call inherit-product-if-exists, vendor/blowpop/$(LOCAL_STEM))
 
